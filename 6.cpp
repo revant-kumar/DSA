2c2e491cf4930d7a7ee88b77bd78a8a679384281
revant-kumar <revantx1816@gmail.com>
2023-08-05 11:53:13 +0530

    Leet-283

---
+++ 6.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> nums = {0,1,0,3,12};
+    int i, n=nums.size(), counter=0;
+    for(i=0;i<n;i++){
+        if(nums[i]!=0){
+            nums[counter] = nums[i];
+            counter++;
+        }
+    }
+
+    for(i=counter;i<n;i++){
+        nums[i] = 0;
+    
+    }
+    for(auto i:nums){
+        cout<<i<<" ";
+    }
+}
