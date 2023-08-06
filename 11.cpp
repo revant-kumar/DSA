6d4aba34de6c5bcb3ea7e3e5c5bfbcfca06d7e9e
revant-kumar <revantx1816@gmail.com>
2023-08-06 19:10:23 +0530

    CN-LowerBound

--- 11.cpp
+++ 11.cpp
@@ -1,37 +1,29 @@
+//CN: Lower Bound using Binary Search
 #include <bits/stdc++.h>
 using namespace std;
 
 int bs(vector<int>& arr, int l, int r, int x){
     int mid = (l+r)/2;
 	if(l<=r){
-		if(arr[mid]==x){
-			return mid;
-		}
-		else if(x>arr[mid]){
-			return bs(arr,mid+1,r,x);
+		if(x<=arr[mid]){
+			return bs(arr,l,mid-1,x);
 		}
 		else{
-		    return bs(arr,l,mid-1,x);
+		    return bs(arr,mid+1,r,x);
 		}
 	}
-	return -1;
+	return l;
 }
 
 int lowerBound(vector<int> arr, int n, int x) {
 	int k = bs(arr,0,n-1,x);
-	if(k==-1){
-		return n;
-	}
-	else{
-		return k;
-	}
+	return k;
 }
 
-
-int main() {
+int main(){
     vector<int> nums = {3,6,11,12,19,20,67}; 
-    int n = sizeof(nums)/sizeof(nums[0]);
+    int n = nums.size();
     
-    int x = 67;
+    int x = 18;
     cout<<"Output: "<<lowerBound(nums,n,x);
 }
