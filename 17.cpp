0142bd585b43f7fdbe26d76e6fc1fcc9f3cf8e2d
revant-kumar <revantx1816@gmail.com>
2023-08-11 13:43:22 +0530

    CN-Number of occurences

--- 17.cpp
+++ 17.cpp
@@ -3,17 +3,17 @@
 using namespace std;
 
 int lowerBound(vector<int>& nums, int l, int r, int x){
-        while(l<=r){
-            int mid = (l+r)/2;
-            if(nums[mid]>=x){
-                r = mid-1; 
-            }
-            else{
-                l = mid+1;
-            }
+    while(l<=r){
+        int mid = (l+r)/2;
+        if(nums[mid]>=x){
+            r = mid-1; 
         }
-        return l;
+        else{
+            l = mid+1;
+        }
     }
+    return l;
+}
 
 int count(vector<int>& arr, int n, int x) {
 	int a = lowerBound(arr,0,n-1,x);
